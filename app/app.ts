40ff81f868cc352438a827eb0b8ca890ed33adf0
wu-jeffrey <j387wu@edu.uwaterloo.ca>
2020-05-28 20:40:25 -0400

    Deploy attempt

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,8 @@
 import * as Express from "express";
+import * as path from "path";
 import { httpErrorHandler } from "./middleware";
 import { sessionRoutes } from "./routes";
+
 export const app = Express();
 
 // Middleware
@@ -8,6 +10,14 @@ app.use(Express.json());
 
 // API routes
 app.use("/api/sessions", sessionRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(Express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Error Handling Middleware
 app.use(httpErrorHandler);
